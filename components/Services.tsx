b53c02ece17512169d230e65827469b9f1e481fa
Shahjahan <md.shahjahan786pubg@gmail.com>
2023-12-17 19:46:21 +0530

    added services components

---
+++ components/Services.tsx
@@ -0,0 +1,54 @@
+import Link from "next/link";
+import React from "react";
+
+interface ServicesProps {
+  blockQuote: String;
+  heading: String;
+  content: string;
+  linkText: String;
+  videoSrc: string;
+}
+
+const Services: React.FC<ServicesProps> = ({
+  blockQuote,
+  heading,
+  content,
+  linkText,
+  videoSrc,
+}) => {
+  return (
+    <div className="m-7">
+      <div
+        className={`p-10 flex flex-col lg:flex-row-reverse justify-center align-middle bg-gradient-to-r from-teal-900 via-teal-600 to-teal-300 rounded-3xl`}
+      >
+        <div className="mt-3 lg:m-16 grid grid-cols-1 gap-7 ">
+          <p className="text-[#C5EA41] text-lg">{blockQuote}</p>
+          <h1 className="text-white text-3xl md:text-5xl lg:text-7xl">
+            {heading}
+          </h1>
+          <div className="text-white text-base md:text-lg lg:text-xl ">
+            {content}
+          </div>
+
+          <div className="text-black grid grid-cols-2 gap-1 hover:gap-2 items-center ">
+            <span className="text-xl ">
+              <Link href="" className="hover:underline" color={"black"}>
+                {linkText}
+              </Link>
+              {">"}
+            </span>
+          </div>
+        </div>
+        <video
+          src={require(`../public/${videoSrc}`)}
+          autoPlay
+          muted
+          loop
+          className="responsive-video"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Services;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import Companies from "@/components/Companies";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Services from "@/components/Services";
+import Services2 from "@/components/Services2";
 
 export default function Home() {
   return (
@@ -10,6 +12,29 @@ export default function Home() {
         <Navbar />
         <Hero />
         <Companies />
+        <Services
+          key={"1"}
+          blockQuote="COMPOSABLE CUSTOMER DATA PLATFORM"
+          heading="Fast, flexible, affordable CDP alternative"
+          content="Distribute customer audiences anywhere you need them with a no-code interface built for marketers. Hightouch is warehouse-first, making it more flexible, more powerful, and easier to set up than a legacy CDP."
+          linkText="Learn more about Customer Studio"
+          videoSrc="defineAudience.webm"
+        />
+        <Services2
+          blockQuote="REVERSE ETL PLATFORM"
+          heading="Sync data to any app without engineering tickets or APIs"
+          content="Tell us what data you want and where you want it. Hightouch automatically does the rest while providing version control, observability, alerting, and more."
+          linkText="Learn more about Reverse ETL"
+          videoSrc="syncdata.webm"
+        />
+        <Services
+          key={"3"}
+          blockQuote="COMPOSABLE CUSTOMER DATA PLATFORM"
+          heading="Fast, flexible, affordable CDP alternative"
+          content="Distribute customer audiences anywhere you need them with a no-code interface built for marketers. Hightouch is warehouse-first, making it more flexible, more powerful, and easier to set up than a legacy CDP."
+          linkText="Learn more about Customer Studio"
+          videoSrc="toolkit.webm"
+        />
         <Footer />
       </div>
     </main>
